536b5aeef8fc72a2c071b11d1b54183018141e94
aliraza114 <aliraza3569@gmail.com>
2024-06-06 10:48:29 +0500

    add error and api call

--- src/app/admin/products/page.tsx
+++ src/app/admin/products/page.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/ui/button";
 import PageHeader from "../_components/PageHeader";
 import Link from "next/link";
+import { Table, TableBody, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 
 export default function AdminProductPage() {
     return <>
@@ -10,9 +11,27 @@ export default function AdminProductPage() {
         <Link href={'/admin/products/new'} >Add Product</Link>
     </Button>
     </div>
+    <ProductsTable />
     </>
 }
 
 function ProductsTable() {
-    return 
+    return <Table>
+        <TableHeader>
+            <TableRow>
+                <TableHead className="w-0">
+                    <span className="sr-only">Available For Purchase</span>
+                </TableHead>
+                <TableHead>Name</TableHead>
+                <TableHead>Price</TableHead>
+                <TableHead>Orders</TableHead>
+                <TableHead className="w-0">
+                    <span className="sr-only">Actions</span>
+                </TableHead>
+            </TableRow>
+        </TableHeader>
+        <TableBody>
+            
+        </TableBody>
+    </Table>
 }
